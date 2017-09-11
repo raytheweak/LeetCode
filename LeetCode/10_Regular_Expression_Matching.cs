70e639df177f5d18ffe2ba2846860a90210dc0fd
wangruiqian <wangruiqian@juxinli.com>
2017-09-11 12:10:34 +0800

    Problem 9-11

---
+++ LeetCode/10_Regular_Expression_Matching.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode
+{
+    public class RegularExpressionMatching
+    {
+        //dynamic programming
+        public bool IsMatch(string s, string p)
+        {
+            int m = s.Length, n = p.Length;
+            //if s[0,..,i-1] matches j[0,...,j-1], then f[i,j] is true.
+            bool[,] f = new bool[m + 1, n + 1];
+
+            f[0, 0] = true;
+            //first we settle f[0~m, 0] and f[0, 0~n].
+            for (int i = 1; i <= m; i++)
+                f[i, 0] = false;
+            // p[0,..., j - 3, j - 2, j - 1] matches empty string if p[j - 1] is '*' and p[0,...,j - 3] matches empty
+            for (int j = 1; j <= n; j++)
+                f[0, j] = j > 1 && '*' == p[j - 1] && f[0, j - 2];
+
+            for (int i = 1; i <= m; i++)
+                for (int j = 1; j <= n; j++)
+                    if (p[j - 1] != '*')
+                        f[i, j] = f[i - 1, j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+                    else
+                        //remove the last two char(that is x*) of pattern, still matches(so x* matches empty).
+                        //or, string adds some more matching chars to the x*.
+                        f[i, j] = f[i, j - 2] || ((s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1, j]);
+
+            return f[m, n];
+        }
+        public static void Test()
+        {
+            var solution = new RegularExpressionMatching();
+            var result = solution.IsMatch("aab", ".*b");
+            System.Diagnostics.Debug.Assert(result == true);
+
+            result = solution.IsMatch("abcd", "d*");
+            System.Diagnostics.Debug.Assert(result == false);
+        }
+    }
+}
---
+++ LeetCode/11_Container_With_Most_Water.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCode
+{
+    public class ContainerWithMostWater
+    {
+        //The lesson is that through some technique, many possibilities can be excluded 
+        //from all the possibilities of the brute force approach.
+        public int MaxArea(int[] height)
+        {
+            int lo = 0, hi = height.Length - 1;
+            int maxArea = 0;
+            while (lo < hi)
+            {
+                if (height[lo] < height[hi])
+                {
+                    int currentArea = height[lo] * (hi - lo);
+                    if (maxArea < currentArea)
+                    {
+                        maxArea = currentArea;
+                    }
+                    lo++;
+                }
+                else
+                {
+                    int currentArea = height[hi] * (hi - lo);
+                    if (maxArea < currentArea)
+                    {
+                        maxArea = currentArea;
+                    }
+                    hi--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
